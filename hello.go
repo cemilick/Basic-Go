614383d24e8a2d04cba4a3493ab7bb2565a0f95d
cemilick <68626024+cemilick@users.noreply.github.com>
2020-07-22 09:52:45 +0700

    File Tugas Pertama
    menampilkan nama kakak dan adik

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var kakak = "Merosa Pesona"
+	var adik = "Anosep Asorem"
+
+	fmt.Println("Nama Kakak : " + kakak)
+	fmt.Println("Nama Adik : " + adik)
+}
