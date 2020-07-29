14e08bbcbad7862efe450d594c030a3307ab4246
cemilick <68626024+cemilick@users.noreply.github.com>
2020-07-29 14:30:18 +0700

    Update hello.go

--- hello.go
+++ hello.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 func main() {
+	// deklarasi variabel kakak dan adik
 	var kakak = "Merosa Pesona"
 	var adik = "Anosep Asorem"
 
+	// menampilkan nama kakak dan adik
 	fmt.Println("Nama Kakak : " + kakak)
 	fmt.Println("Nama Adik : " + adik)
 }
